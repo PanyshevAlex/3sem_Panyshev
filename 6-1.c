94ef15220884a7018b74179c3501bf15912965f5
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2020-10-15 16:26:04 +0300

    Update 6-1.c

--- 6-1.c
+++ 6-1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+void is_type(file_mode)
+{
+	if (S_ISREG(file_mode))
+		printf("%-20s", "REGULAR");
+	if (S_ISDIR(file_mode))
+		printf("%-20s", "DIRECTORY");
+	if (S_ISCHR(file_mode))
+		printf("%-20s", "CHARACTER SPECIAL");
+	if (S_ISBLK(file_mode))
+		printf("%-20s", "BLOCK SPECIAL");
+	if (S_ISFIFO(file_mode))
+		printf("%-20s", "FIFO");
+	if (S_ISLNK(file_mode))
+		printf("%-20s", "SYMBOLIC LINK");
+	if (S_ISSOCK(file_mode))
+		printf("%-20s", "SOCKET");
+	if (S_ISWHT(file_mode))
+		printf("%-20s", "WHITEOUT");
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +37,15 @@ int main(int argc, char *argv[])
 		return 2;
 	}
  	struct dirent *dir;
-
+	struct stat stat_buf;
+	printf("%-20s%s\n", "type", "name");
 	while ((dir = readdir(dir_str)) != NULL){
+		if (lstat(dir->d_name, &stat_buf) == -1)
+		{
+			perror("Failed to stat");
+			return 2;
+		}	
+		is_type(stat_buf.st_mode);
 		printf("%s\n", dir->d_name);
 	}
 		
